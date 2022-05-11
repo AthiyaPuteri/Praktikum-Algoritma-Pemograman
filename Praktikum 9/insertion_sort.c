25c44ae4b46d0e46b72d68e42c6adfa0fd883e96
AthiyaPuteri <100354505+AthiyaPuteri@users.noreply.github.com>
2022-05-11 23:26:59 +0700

    Add files via upload

---
+++ Praktikum 9/insertion_sort.c
@@ -0,0 +1,37 @@
+// Nama : Athiya Puteri Hidayat
+// NIM : 24060121140128
+// Nama file : insertion_sort.c
+// Deskripsi : Membuat algoritma pengurutuan dengan cara membandingkan dan mengurutkan dua data pertama pada array
+
+#include <stdio.h>
+#include "fungsi_arr.c"
+
+void insertionSort(int arr[], int n){
+    //Kamus lokal
+    int i, j, key;
+
+    //Algoritma
+    for (i = 1; i < n; i++){
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main(){
+    //Kamus lokal
+    int arr[]={47,25,51,7,93,45,1,69,57,29};
+    int n;
+
+    //Algoritma
+    n = sizeof(arr) / sizeof(arr[0]);
+    printArr(arr, n);
+    insertionSort(arr, n);
+    printArr(arr, n);
+
+    return 0;
+}
